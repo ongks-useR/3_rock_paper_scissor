3c98a15b81fbc2530b1f1846fe211b62eee60836
ongks <45529850+ongks-useR@users.noreply.github.com>
2024-01-25 09:58:02 +0800

    Create function getPlayerChoice that identical to getComputerChoice. playRound function that take output from getPlayerChoice and getComputerChoice to determine result of the game.

--- game.js
+++ game.js
@@ -14,3 +14,39 @@ function getComputerChoice() {
     }
     return result;
 }
+
+function getPlayerChoice() {
+
+    let num = Math.ceil(Math.random() * 3);
+    let result;
+
+    if (num === 1) {
+        result = "Rock"
+    }
+    else if (num === 2) {
+        result = "Scissor"
+    }
+    else {
+        result = "Paper"
+    }
+    return result;
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    const player = playerSelection;
+    const computer = computerSelection;
+
+    let victory = (player === "Rock" & computer === "Scissor") || (player === "Scissor" & computer === "Paper") || (player === "Paper" & computer === "Rock")
+
+    let result;
+
+    if (victory) {
+        result = "You WIN!!"
+    }
+    else {
+        result = "You LOSE!!"
+    }
+
+    return result;
+}
