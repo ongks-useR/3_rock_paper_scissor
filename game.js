8070e26535e217289b67d417b3eaa362e9335ca7
ongks <45529850+ongks-useR@users.noreply.github.com>
2024-01-25 14:29:13 +0800

    new function - game, to officially play the game.

--- game.js
+++ game.js
@@ -37,6 +37,7 @@ function playRound(playerSelection, computerSelection) {
     const player = playerSelection;
     const computer = computerSelection;
 
+    /* Scenarios where Player win the game */
     let victory = (player === "Rock" & computer === "Scissor") || (player === "Scissor" & computer === "Paper") || (player === "Paper" & computer === "Rock")
 
     let result;
@@ -49,4 +50,37 @@ function playRound(playerSelection, computerSelection) {
     }
 
     return result;
+}
+
+/* 
+    Ultimate function that plays the game where
+    user will decide how many game to play. if user score more than computer, then user wins and
+    vice versa.
+*/
+function game() {
+
+    let n = parseInt(prompt("How many round you wanna play?"));
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 1; i <= n; i++) {
+
+        const outcome = playRound(getPlayerChoice(), getComputerChoice());
+
+        console.log(outcome)
+
+        if (outcome === "You WIN!!") {
+            playerScore++;
+        }
+        else {
+            computerScore++;
+        }
+    }
+
+    if (playerScore > computerScore) {
+        console.log("You are the WINNER!!");
+    }
+    else {
+        console.log("You are the LOSER!!");
+    }
 }
