755897233c40ba4c1225547df2139d9c80e79858
ongks-useR <45529850+ongks-useR@users.noreply.github.com>
2025-01-07 01:44:42 +0000

    1. new variables playerScore and computerScore to keep track of score; 2. improve logic in playerChoice function such that player only allowed to make a choice after start button is activated.

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 
 let roundNumber = 1;
+let playerScore = 0;
+let computerScore = 0;
 
 const startButton = document.querySelector('input');
 
@@ -28,19 +30,67 @@ function computerChoice(){
     const num = randomNumber();
     const selected = choice[num];
 
-    const computerImg = document.querySelector(".computer");
+    const computerImg = document.querySelector(".selected.computer");
     computerImg.src = selected;
+
+    return num;
 }
 
 
 function playerChoice(e) {
-    const num = e.target.getAttribute('data');
-    const selected = choice[num];
 
-    const playerImg = document.querySelector(".player");
-    playerImg.src = selected;
+    let start = document.querySelector("input[type=button]");
+    start = start.getAttribute('style');
 
-    computerChoice();
+    if (start) {
+        const playerSelection = e.target.getAttribute('data');
+        const selected = choice[playerSelection];
+    
+        const playerImg = document.querySelector(".selected.player");
+        playerImg.src = selected;
+    
+        const computerSelection = computerChoice();
+    
+        // Scenarios where player win
+        const playerWin = (playerSelection == 0 && computerSelection == 2) || (playerSelection == 1 && computerSelection == 0) || (playerSelection == 2 && computerSelection == 1)
+    
+        // Scenarios where computer win
+        const computerWin = (computerSelection == 0 && playerSelection == 2) || (computerSelection == 1 && playerSelection == 0) || (computerSelection == 2 && playerSelection == 1)
+    
+        if (playerWin) {
+            playerScore++
+            const score = document.querySelector(".score.player");
+            score.textContent = playerScore;
+        }
+        else if (computerWin) {
+            computerScore++
+            const score = document.querySelector(".score.computer");
+            score.textContent = computerScore;
+        }
+        else {
+            // Draw, hence nothing will happen to the score
+            return
+        }
+    
+    
+        const result = document.querySelector("#result > p");
+    
+        if (playerScore == 5) {
+            result.textContent = "You WIN !!"
+        }
+        else if (computerScore == 5) {
+            result.textContent = "YOU LOSE !!"
+        }
+        else {
+            roundNumber++
+            const para = document.querySelector('#round > p');
+            para.textContent = `ROUND ${roundNumber}`
+        }
+    }
+    else {
+        console.log("Click is disabled !!")
+    }
+    
 }
 
 const selections = Array.from(document.querySelectorAll("#choice-buttons > img"));
